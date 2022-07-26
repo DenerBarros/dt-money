0615d997178c94bc361ba554fe9df295afe6b2f0
DenerBarros <denerbarros.eng@gmail.com>
2022-07-26 10:20:59 -0300

    Criação de uma API fake com mirage para não deixar os dados estátios

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createServer } from 'miragejs';
 import { App } from './App';
+
+createServer({
+  routes() {
+    this.namespace = 'api'
+
+    this.get('/transaction', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1', 
+          amout: 400,
+          type: 'deposit',
+          category: 'Food',
+          createAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 
 const root = ReactDOM.createRoot(
