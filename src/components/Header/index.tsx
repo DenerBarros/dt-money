d6e1c570fa191cac2f60b1bdfa0baeec3d7d90eb
DenerBarros <denerbarros.eng@gmail.com>
2022-07-26 10:55:29 -0300

    Configuração do Modal e passando propriedades de elemento filho para pai para setar estado

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,26 +3,20 @@ import logoImg from '../../assets/logo.svg';
 import Modal from 'react-modal';
 import { Container, Content } from './styles';
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewransactionModal: () => void;
+}
 
-  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
-
-  function handleOpenNewTransactionModal(){
-    setIsNewTransactionModalOpen(true);
-  }
-
-  function handleCloseNewTransactionModal(){
-    setIsNewTransactionModalOpen(false);
-  }
-
+export function Header({ onOpenNewransactionModal }: HeaderProps) {
 
   return (
     <Container>
         <Content>
             <img src={logoImg} alt="dbmoney" />
-            <button type="button" onClick={handleOpenNewTransactionModal}>
+            <button type="button" onClick={onOpenNewransactionModal}>
                 Nova Transação
             </button>
+           
         </Content>
     </Container>
   )
