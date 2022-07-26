305b89ec4698e09214d92ece83dc70ea5747c731
DenerBarros <denerbarros.eng@gmail.com>
2022-07-26 09:16:51 -0300

    Correção dos Menus do Dashboard

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -21,7 +21,7 @@ export const Content = styled.div`
         padding: 0 2rem;
         border-radius: 0.25rem;
         height: 3rem;
-        transition: 0.2s;
+        transition: 0.3s;
         
         &:hover {
             filter: brightness(0.9);
