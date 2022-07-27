13ba3257202f50bb950815f0b2c1f68a159aaeac
DenerBarros <denerbarros.eng@gmail.com>
2022-07-27 09:43:25 -0300

    Estilização do Modal - Adicionando efeito nos botões de cadastrar e fechar

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,6 @@
 import Modal from 'react-modal';
 import { Container } from './styles';
+import closeImg from "../../assets/close.svg";
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -14,6 +15,13 @@ export function NewTransactionModal({ isOpen, onRequestClose } : NewTransactionM
         overlayClassName="react-modal-overlay"
         className="react-modal-content"
       >
+
+        <button type="button"
+        onClick={onRequestClose} 
+        className="react-modal-close "
+        >
+            <img src={closeImg} alt="Fechar Modal" />
+        </button>
         <Container>
         <h2>Cadastrar Transação</h2>
 
